9427c836b7f8f42c9a3aabd44eac8ef35a4aa7b3
ancutza27 <cristianancutza@hotmail.com>
2013-10-01 16:33:51 -0300

    tp1

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,14 +4,14 @@ package edu.tallerweb.pptls;
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
-
+	int valorForma;
 	/**
 	 * Toda Mano debe crearse con una forma dada, que será
 	 * la que determine su condición en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		valorForma = forma.getValor();
 	}
 
 	/**
@@ -21,7 +21,25 @@ public class Mano {
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
-	}
+		   if( valorForma == 0 && otra.valorForma == 3 || valorForma == 0 && otra.valorForma == 2 
+					|| valorForma == 1 && otra.valorForma == 0 || valorForma == 1 && otra.valorForma == 4 
+					|| valorForma == 2 && otra.valorForma == 1 || valorForma == 2 && otra.valorForma == 3 
+					|| valorForma == 3 && otra.valorForma == 4 || valorForma == 3 && otra.valorForma == 1 
+					|| valorForma == 4 && otra.valorForma == 2 || valorForma == 4 && otra.valorForma == 0 )
+					   
+					   return Resultado.GANA;
+				   
+				   if( valorForma == 0 && otra.valorForma == 4 || valorForma == 0 && otra.valorForma == 1 
+						|| valorForma == 1 && otra.valorForma == 2 || valorForma == 1 && otra.valorForma == 3 
+						|| valorForma == 2 && otra.valorForma == 4 || valorForma == 2 && otra.valorForma == 0 
+						|| valorForma == 3 && otra.valorForma == 0 || valorForma == 3 && otra.valorForma == 2 
+						|| valorForma == 4 && otra.valorForma == 3 || valorForma == 4 && otra.valorForma == 1 )
+					   
+					   return Resultado.PIERDE;
+				   
+				   else
+				       return Resultado.EMPATA;
+				}
+	
 
 }
